ccf407d69b15996b43471152052847faced3bc90
ksoniak <sofya1604@inbox.ru>
2026-04-08 11:26:19 +0300

    Задание 2.2: реализован класс BankTransfer, CreditCard, CryptoWallet, PaymentProcessor.

--- part2/part2_2/PaymentProcessor.java
+++ part2/part2_2/PaymentProcessor.java
@@ -31,7 +31,11 @@ public class PaymentProcessor {
      */
     public static void describe(PaymentMethod pm) {
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-
+        switch (pm) {
+            case CreditCard cc -> System.out.println("  Держатель карты: " + cc.holder());
+            case BankTransfer bt -> System.out.println("  Банк: " + bt.bankName() + ", IBAN: " + bt.iban());
+            case CryptoWallet cw -> System.out.println("  Кошелёк: " + cw.currency() + ", Валюта: " + cw.currency());
+        }
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 }
